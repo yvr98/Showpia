13e4a486fda4ee463ad02ed479eb5a217ce5319f
yvr98 <yudthesvar98@gmail.com>
2023-06-18 16:35:36 -0400

    Added switch genre functionality

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,10 @@ import React, {useEffect} from 'react'
 import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box, CircularProgress } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
+import { useGetGenresQuery} from '../../services/TMBD';
+import genreIcons from '../../assets/genres';
 import useStyles from './styles';
 
 const  categories = [
@@ -9,20 +13,20 @@ const  categories = [
     {label:'Top Rated', value:'top_rated'},
     {label:'Upcoming', value:'upcoming'},
 ];
-const  demoCategories = [
-    {label:'Comedy', value:'comedy'},
-    {label:'Action', value:'action'},
-    {label:'Horror', value:'horror'},
-    {label:'Animation', value:'animation'},
-];
+
 
 
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 const Sidebar = ({setMobileOpen}) => {
+const {  genreIdOrCategoryName } = useSelector((state) => state.currentGenreOrCategory);
 const theme = useTheme();
 const classes = useStyles();
+const { data, isFetching } = useGetGenresQuery();
+const dispatch = useDispatch();
+
+
   return (
     <>
         <Link to="/" className={classes.imageLink}>
@@ -36,10 +40,10 @@ const classes = useStyles();
             <ListSubheader>Categories</ListSubheader>
             {categories.map(({label,value})=>(
                <Link key={value} className={classes.links} to="/">
-                <ListItem onClick={()=>{}} button>
-                    {/* <ListItemIcon>
-                        <img src={redLogo} className={classes.genreImages} height={30}/>
-                    </ListItemIcon> */}
+                <ListItem onClick={()=>dispatch(selectGenreOrCategory(value))} button>
+                   <ListItemIcon>
+                        <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30}/>
+                    </ListItemIcon> 
                     <ListItemText primary={label}/>
                 </ListItem>
                </Link>
@@ -48,13 +52,17 @@ const classes = useStyles();
         <Divider/>
         <List>
             <ListSubheader>Genres</ListSubheader>
-            {demoCategories.map(({label,value})=>(
-               <Link key={value} className={classes.links} to="/">
-                <ListItem onClick={()=>{}} button>
-                    {/* <ListItemIcon>
-                        <img src={redLogo} className={classes.genreImages} height={30}/>
-                    </ListItemIcon> */}
-                    <ListItemText primary={label}/>
+            {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box> ):
+            data.genres.map(({name,id})=>(
+               <Link key={name} className={classes.links} to="/">
+                <ListItem onClick={()=>dispatch(selectGenreOrCategory(id))} button>
+                <ListItemIcon>
+                    <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30}/>
+                </ListItemIcon> 
+                    <ListItemText primary={name}/>
                 </ListItem>
                </Link>
             ))}
--- src/services/TMBD.js
+++ src/services/TMBD.js
@@ -7,14 +7,34 @@ export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
   endpoints: (builder) => ({
- 
+  
+     //* Get Genres
+     getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
     //* Get Movies by [Type]
     getMovies: builder.query({
-      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}` ,
+      query: ({genreIdOrCategoryName,page}) => {
+
+        //*get movies by category
+        if(genreIdOrCategoryName && typeof genreIdOrCategoryName ==='string'){
+           return `movie/${genreIdOrCategoryName}?page=${page}&api_key=${tmdbApiKey}`
+        }
+
+       
+        //*get movies by genre
+        if(genreIdOrCategoryName && typeof genreIdOrCategoryName ==='number'){
+          return `discover/movie?with_genres=${genreIdOrCategoryName}&page=${page}&api_key=${tmdbApiKey}`
+        }
+        //*get popular movies
+        return   `movie/popular?page=${page}&api_key=${tmdbApiKey}` ;
+      },
+    
       }), 
     }),
 });
 
 export const {
- useGetMoviesQuery
+ useGetMoviesQuery,
+ useGetGenresQuery,
 } = tmdbApi;
