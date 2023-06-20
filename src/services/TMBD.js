b3a366ccd5a2eeefb0795d781969b6a0c06256d7
yvr98 <yudthesvar98@gmail.com>
2023-06-20 14:22:01 -0400

    Implemented Actor paege and movies actor is in

--- src/services/TMBD.js
+++ src/services/TMBD.js
@@ -43,7 +43,13 @@ export const tmdbApi = createApi({
       //*get user specific list
       getRecommendations : builder.query({
         query:({movie_id,list})=> `/movie/${movie_id}/${list}?api_key=${tmdbApiKey}`
-      })
+      }),
+      getActorsDetails: builder.query({
+        query: (id) => `person/${id}?api_key=${tmdbApiKey}`,
+      }),
+      getMoviesByActorId: builder.query({
+        query: ({ id, page }) => `/discover/movie?with_cast=${id}&page=${page}&api_key=${tmdbApiKey}`,
+      }),
     }),
 });
 
@@ -52,4 +58,6 @@ export const {
  useGetGenresQuery,
  useGetMovieQuery,
  useGetRecommendationsQuery,
+ useGetActorsDetailsQuery,
+ useGetMoviesByActorIdQuery,
 } = tmdbApi;
